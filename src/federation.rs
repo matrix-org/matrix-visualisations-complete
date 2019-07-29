5e3c1a3e0e153301e66981c2a5cec1e625c848f6
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-07-29 12:00:13 +0200

    federation.rs: Add functions for signing requests

--- src/federation.rs
+++ src/federation.rs
@@ -49,3 +49,42 @@ pub fn serv_cert(_: web::Path<String>) -> Box<dyn Future<Item = HttpResponse, Er
             .body("The cert of the server"),
     ))
 }
+
+fn request_json(
+    method: &str,
+    origin_name: &str,
+    origin_key: &SecretKey,
+    key_name: &str,
+    destination: &str,
+    path: &str,
+    content: Option<JsonValue>,
+) -> String {
+    let json_to_sign = RequestJson {
+        method: method.to_string(),
+        uri: path.to_string(),
+        origin: origin_name.to_string(),
+        destination: destination.to_string(),
+        content,
+    };
+
+    let bytes = make_canonical(json_to_sign).expect("Failed make_compact");
+    let signature = sodiumoxide::crypto::sign::ed25519::sign_detached(&bytes, origin_key);
+    let base64_signature = base64::encode_config(&signature, base64::STANDARD_NO_PAD);
+
+    format!(
+        r#"X-Matrix origin={},key="{}",sig="{}""#,
+        origin_name, key_name, base64_signature
+    )
+}
+
+fn make_canonical(s: impl Serialize) -> Result<Vec<u8>, Error> {
+    let value = serde_json::to_value(s)?;
+    let uncompact = serde_json::to_vec(&value)?;
+
+    let mut canonical = Vec::with_capacity(uncompact.len());
+    indolentjson::compact::compact(&uncompact, &mut canonical).expect("Invalid JSON");
+
+    let canonical = String::from_utf8(canonical).expect("Failed to parse canonical");
+
+    Ok(canonical.into_bytes())
+}
